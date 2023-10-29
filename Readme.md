4e0c3dd48a10cc7f3230a6579e831f1f7a50923f
Jiraplus0304 <jiraplus.chan@mail.kmutt.ac.th>
2023-10-29 21:46:05 +0700

    Updated Filed

--- Readme.md
+++
@@ -1,1 +0,0 @@
-Java it's so hard
--- classicmodelstue/src/main/java/sit/int202/classicmodelstue/TestEve.java
+++
@@ -1,8 +0,0 @@
-package sit.int202.classicmodelstue;
-
-public class TestEve {
-    public static void main(String[] args) {
-        int x= (int) ((Math.random()*5000)+1);
-        System.out.println(x);
-    }
-}
