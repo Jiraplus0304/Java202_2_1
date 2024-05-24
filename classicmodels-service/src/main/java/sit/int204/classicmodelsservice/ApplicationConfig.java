767e6766afcaf96990dbbe3487b37df4d23102ef
Jiraplus0304 <jiraplus.chan@mail.kmutt.ac.th>
2024-05-24 13:09:41 +0700

    Update classicmodels

---
+++ classicmodels-service/src/main/java/sit/int204/classicmodelsservice/ApplicationConfig.java
@@ -0,0 +1,33 @@
+package sit.int204.classicmodelsservice;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import sit.int204.classicmodelsservice.services.ListMapper;
+
+import java.util.List;
+
+//@Configuration
+//public class ApplicationConfig {
+//    @Bean
+//    public ModelMapper modelMapper() {
+//        return new ModelMapper();
+//    }
+//    @Bean
+//    public ListMapper listMapper() {
+//        return ListMapper.getInstance();
+//    }
+//}
+
+@Configuration
+public class ApplicationConfig{
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+    @Bean
+    public ListMapper listMapper(){
+        return ListMapper.getInstance();
+    }
+}
+
